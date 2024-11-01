40138b2cdb60890483869e9cdf4bc4f8cbc39bef
Micael Macedo <micaelm2009@hotmail.com>
2024-11-01 17:06:02 -0300

    feat: add component life-cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { DataBindingComponent } from "./data-binding/data-binding.component";
+import { CicloComponent } from "./ciclo/ciclo.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataBindingComponent],
+  imports: [RouterOutlet, DataBindingComponent, CicloComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'data-binding';
+  title = 'data-binding'
+
+  deletarCiclo: boolean = false
+
+  valor:number = 5
+
+  mudarValor(){
+    this.valor++
+    console.log(this.valor)
+  }
+
+  destruirCiclo(){
+    this.deletarCiclo = true
+  }
 }
